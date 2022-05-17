1a132fb34c89d951c153acc86d11c181404479cc
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-17 13:29:19 +0200

    add title et formulaire

--- src/views/pages/login.php
+++ src/views/pages/login.php
@@ -1,6 +1,30 @@
 <?php ob_start(); ?>
 
 <h1>Login</h1>
+<div class="container ">
+  <div class="row align-items-center">
+    <h2 class="mb-5 mt-5 text-center">Bienvenue à la COGIP</h2>
+  </div>
+  <div class="row align-items-center">
+    <h3 class="mb-5 mt-3 text-center"> Bienvenue dans l’espace de connexion de la COGIP</h3>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-6">
+      <form action="" method="post">
+        <div class="mb-3">
+          <label for="identifiant" class="form-label">Votre identifiant</label>
+          <input type="text" class="form-control" id="" name="" aria-describedby="identifiant" placeholder="Jean-Christian">
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Votre mot de passe</label>
+          <input type="password" class="form-control" id="password" name="">
+        </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    </div>
+    
+  </div>
+</div>
 
 <?php 
 $content = ob_get_clean();
