fa8b2cdce309760b1b1f1b7b995d078f70a740d5
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-17 10:00:22 +0200

    add header

--- src/views/elements/header.php
+++ src/views/elements/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,6 +9,40 @@
         <title>Document</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="../../../public/assets/style/style.css">
     </head>
     <body>
-        <p>Je suis le header</p>
+        <header>
+           <nav class="mon_header navbar  navbar-expand-lg navbar-light  ">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="../../assets/img/logo.svg" alt="Logo" width="30" height="24">
+                    <span >COGIP</span>
+                </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav mb-2 gap-3 ">
+                    <li class="nav-item  " >
+                        <a class="nav-link active"  aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link" href="#">Factures</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link"  href="#">Sociétés</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link"  href="#">Contacts</a>
+                    </li>
+                    <li class="nav-item" >
+                        <a class="nav-link"  href="#">Connexion</a>
+                    </li>
+        
+                </ul>
+            </div>
+        </div>
+        </nav> 
+        </header>
+        <p>Je suis le header user</p>
