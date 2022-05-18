9e32b766a017df62ebe260383d6c27170829c30e
Nadison Fidely <fidelysnad@exemple.com>
2022-05-18 14:39:41 +0200

    réctification

--- src/views/pages/invoiceDetails.php
+++ src/views/pages/invoiceDetails.php
@@ -1,33 +1,6 @@
 <?php ob_start(); ?>
 
-<h1>Facture : <span></span></h1>
-<table>
-    <caption>Société liée à la factures</caption>
-    <thead>
-        <tr>
-            <th>Nom</th>
-            <th>TVA</th>
-            <th>Type société</th>
-        </tr>
-    </thead>
-    <tbody>
-    
-    </tbody>
-</table>
-
-<table>
-    <caption>Personne de contact</caption>
-    <thead>
-        <tr>
-            <th>Nom</th>
-            <th>Email</th>
-            <th>Phone</th>
-        </tr>
-    </thead>
-    <tbody>
-    
-    </tbody>
-</table>
+<h1>Invoice Details</h1>
 
 <?php 
 $content = ob_get_clean();
