d26194f386562324ffdac376929ac2bda6a4cac5
Nadison Fidely <fidelysnad@exemple.com>
2022-05-18 16:15:38 +0200

    html menuAdmin fait

--- src/views/pages/adminWelcome.php
+++ src/views/pages/adminWelcome.php
@@ -1,7 +1,42 @@
 <?php ob_start(); ?>
 
-<h1>Welcome admin</h1>
+<h1>Bienvenue à la COGIP</h1>
+<p>Bonjour Jean Christian !</p>
+<p>Que souhaitez-vous faire aujourd'hui ?</p>
 
+<button>Nouvelle facture</button>
+<button>Nouveau contact</button>
+<button>Nouvelle société</button>
+
+<table>
+    <caption>Dérnieres factures :</caption>
+    <thead>
+        <tr>
+            <th>Numéro facture</th>
+            <th>Dates</th>
+            <th>Société</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+
+
+<table>
+    <caption>Dérnieres contact :</caption>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Téléphone</th>
+            <th>e-mail</th>
+            <th>Société</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
 <?php
 $content = ob_get_clean();
 require __DIR__. '/../layout.php';
