7c52aaef72ec4a477147e1e71c9b1467c88ac0e7
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-18 11:05:07 +0200

    add tableau pour invoices

--- src/views/pages/invoices.php
+++ src/views/pages/invoices.php
@@ -1,6 +1,39 @@
 <?php ob_start(); ?>
 
-<h1>List Invoices</h1>
+<!-- <h1>List Invoices</h1> -->
+
+<div class="container ">
+  <div class="row  mb-5 mt-5">
+    <h2 class="text-center">COGIP : Listing des factures</h2>
+  </div>
+  
+  <div class="row justify-content-center ">
+    <div class="col-8 mt-5 ">
+      <table class="table table-bordered">
+      <!-- Afficher la liste des invoices -->
+        <thead>
+          <tr>
+            <th class="text-center fw-bold">Numero facture</th>
+            <th class="text-center fw-bold">Dates</th>
+            <th class="text-center fw-bold">Société</th>
+            <th class="text-center fw-bold">Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- loop  -->
+          <tr>
+            <td><a href="#"></a></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>     
+
+
+</div>
 
 <?php 
 $content = ob_get_clean();
