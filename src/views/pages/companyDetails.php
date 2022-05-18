b44e7db35885d927dad0f89852ddefca53d8f335
Nadison Fidely <fidelysnad@exemple.com>
2022-05-18 12:32:27 +0200

    les table sont fait

--- src/views/pages/companyDetails.php
+++ src/views/pages/companyDetails.php
@@ -1,6 +1,36 @@
 <?php ob_start(); ?>
 
-<h1>Company Details</h1>
+<h1>Société : <span></span> </h1>
+<p>N° TVA : <span></span> </p>
+<p>Type : Client</p>
+
+<table>
+    <caption>Personnes de contact</caption>
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Phone</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+
+<table>
+    <caption>Factures</caption>
+    <thead>
+        <tr>
+            <th>N° facture</th>
+            <th>Date</th>
+            <th>Personne de contact</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
 
 <?php
 $content = ob_get_clean();
