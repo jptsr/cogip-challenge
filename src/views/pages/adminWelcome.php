012f78aff35027c606673a7acd54394d2e6c811d
Fidelys <nadisonsolojeanfidelys@gmail.com>
2022-05-19 11:20:30 +0200

    ajout image

--- src/views/pages/adminWelcome.php
+++ src/views/pages/adminWelcome.php
@@ -1,20 +1,41 @@
 <?php ob_start(); ?>
 
-<h1>Bienvenue à la COGIP</h1>
+<h1 class="text-center" >Bienvenue à la COGIP</h1>
 <p>Bonjour Jean Christian !</p>
 <p>Que souhaitez-vous faire aujourd'hui ?</p>
 
-<button>Nouvelle facture</button>
+<button>Nouvelle facture <img src="" alt=""> </button>
 <button>Nouveau contact</button>
 <button>Nouvelle société</button>
 
-<table>
-    <caption>Dérnieres factures :</caption>
+<table class= "table table-bordered caption-top">
+    <caption class= "mb-3 fs-2" >Dérnieres factures :</caption>
     <thead>
         <tr>
-            <th>Numéro facture</th>
-            <th>Dates</th>
-            <th>Société</th>
+            <th class="text-center" >Numéro facture</th>
+            <th class="text-center" >Dates</th>
+            <th class="text-center" >Société</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+    </tbody>
+</table>
+
+
+<table class= "table table-bordered caption-top" >
+    <caption class= "mb-3 fs-2" >Dérnieres contact :</caption>
+    <thead>
+        <tr>
+            <th class="text-center">Nom</th>
+            <th class="text-center">Téléphone</th>
+            <th class="text-center">e-mail</th>
+            <th class="text-center">Société</th>
         </tr>
     </thead>
     <tbody>
@@ -22,21 +43,26 @@
     </tbody>
 </table>
 
-
-<table>
-    <caption>Dérnieres contact :</caption>
+<table class= "table table-bordered caption-top">
+    <caption class= "mb-3 fs-2" >Dérnieres factures :</caption>
     <thead>
         <tr>
-            <th>Nom</th>
-            <th>Téléphone</th>
-            <th>e-mail</th>
-            <th>Société</th>
+            <th class="text-center" >Nom</th>
+            <th class="text-center" >TVA</th>
+            <th class="text-center" >Pays</th>
+            <th class="text-center" >Type</th>
         </tr>
     </thead>
     <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
 
     </tbody>
 </table>
+
 <?php
 $content = ob_get_clean();
 require __DIR__. '/../layout.php';
