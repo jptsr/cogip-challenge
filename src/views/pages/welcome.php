70d4177929f9e42cce0c1589a923b0cdfb148bf1
Fidelys <nadisonsolojeanfidelys@gmail.com>
2022-05-19 16:09:41 +0200

    c'est fait

--- src/views/pages/welcome.php
+++ src/views/pages/welcome.php
@@ -1,53 +1,67 @@
 <?php ob_start(); ?>
+<div class="container">
+    <h1 class="text-center" >Bienvenue à la COGIP</h1>
+    <p>Bonjour !</p> 
 
-<h1>Bienvenue à la COGIP</h1>
-<p>Bonjour !</p> 
+    <table class= "table table-bordered caption-top" >
+        <caption class= "mb-3 fs-2">Dernières factures :</caption>
+        <thead>
+            <tr>
+                <th class="text-center" >Numéro facture</th>
+                <th class="text-center" >Dates</th>
+                <th class="text-center" >Société</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table> 
 
-<table>
-    <caption>Dernières factures :</caption>
-    <thead>
-        <tr>
-            <th>Numéro facture</th>
-            <th>Dates</th>
-            <th>Société</th>
-        </tr>
-    </thead>
-    <tbody>
-      
-    </tbody>
-</table>
 
+    <table class= "table table-bordered caption-top">
+        <caption class= "mb-3 fs-2">Dernières contact :</caption>
+        <thead>
+            <tr>
+                <th class="text-center" >Nom </th>
+                <th class="text-center" >Téléphone</th>
+                <th class="text-center" >e-mail</th>
+                <th class="text-center" >Société</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
 
-<table>
-    <caption>Dernières contact :</caption>
-    <thead>
-        <tr>
-            <th>Nom </th>
-            <th>Téléphone</th>
-            <th>e-mail</th>
-            <th>Société</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-    </tbody>
-</table>
-
-<table>
-    <caption>Dernières sociétés :</caption>
-    <thead>
-        <tr>
-            <th>Nom </th>
-            <th>TVA</th>
-            <th>Pays</th>
-            <th>Type</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-    </tbody>
-</table>
-
+    <table class= "table table-bordered caption-top">
+        <caption class= "mb-3 fs-2">Dernières sociétés :</caption>
+        <thead>
+            <tr>
+                <th class="text-center" >Nom </th>
+                <th class="text-center" >TVA</th>
+                <th class="text-center" >Pays</th>
+                <th class="text-center" >Type</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 
 
