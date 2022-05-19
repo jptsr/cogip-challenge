294e7028f42bd25bd9a20f9cb32b6e04e715cdf3
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-19 14:00:51 +0200

    modifier lien pour css

--- src/views/elements/header.php
+++ src/views/elements/header.php
@@ -10,14 +10,14 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         
-        <link rel="stylesheet" href="../../../public/assets/style/style.css">
+        <link rel="stylesheet" href="./assets/style/style.css">
     </head>
     <body>
         <header>
             <nav class="mon_header navbar  navbar-expand-lg navbar-light  ">
                 <div class="container">
                     <a class="navbar-brand" href="#">
-                        <img src="../../assets/img/logo.svg" alt="Logo" width="30" height="24">
+                        <img src="./assets/img/logo.svg" alt="Logo" width="30" height="24">
                         <span >COGIP</span>
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
