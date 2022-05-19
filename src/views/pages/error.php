4074e9e3ebcbb82d9112591a4056916e103d09b9
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-19 11:11:40 +0200

    add message

--- src/views/pages/error.php
+++ src/views/pages/error.php
@@ -1,6 +1,9 @@
 <?php ob_start(); ?>
 
-<h1>Error</h1>
+<h1 class="text-center text-danger mt-5">Attention!</h1>
+<h2 class="text-center mt-5">Les informations envoyées ne permettent pas de vous identifier</h2>
+
+<a class="text-dark" href="/connexion">Connexion</a>
 
 <?php
 $content = ob_get_clean();
