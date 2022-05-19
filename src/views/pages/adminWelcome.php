1931024ee3e749b30516bad510e94311310ae48e
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-19 13:54:16 +0200

    add image poubelle

--- src/views/pages/adminWelcome.php
+++ src/views/pages/adminWelcome.php
@@ -8,13 +8,14 @@
 <button><img src="./assets/img/plus.png" alt="plus" width="25" height="25">Nouveau contact </button>
 <button><img src="./assets/img/plus.png" alt="plus" width="25" height="25">Nouvelle société</button>
 
-<table class= "table table-bordered caption-top">
+<table class= "table table-bordered caption-top mt-5">
     <caption class= "mb-3 fs-2" >Dérnieres factures :</caption>
     <thead>
         <tr>
             <th class="text-center" >Numéro facture</th>
             <th class="text-center" >Dates</th>
             <th class="text-center" >Société</th>
+            <th class="text-center" ></th>
         </tr>
     </thead>
     <tbody>
@@ -22,13 +23,14 @@
             <td></td>
             <td></td>
             <td></td>
+            <td class="text-center"><button class="btn btn-light"><img src="./assets/img/poubelle.png" alt="poubell" width="20" height="20" ></button></td>
         </tr>
 
     </tbody>
 </table>
 
 
-<table class= "table table-bordered caption-top" >
+<table class= "table table-bordered caption-top mt-5" >
     <caption class= "mb-3 fs-2" >Dérnieres contact :</caption>
     <thead>
         <tr>
@@ -36,14 +38,21 @@
             <th class="text-center">Téléphone</th>
             <th class="text-center">e-mail</th>
             <th class="text-center">Société</th>
+            <th class="text-center"></th>
         </tr>
     </thead>
     <tbody>
-
+         <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="text-center"><button class="btn btn-light"><img src="./assets/img/poubelle.png" alt="poubell" width="20" height="20" ></button></td>
+        </tr>
     </tbody>
 </table>
 
-<table class= "table table-bordered caption-top">
+<table class= "table table-bordered caption-top mt-5">
     <caption class= "mb-3 fs-2" >Dérnieres société :</caption>
     <thead>
         <tr>
@@ -51,6 +60,7 @@
             <th class="text-center" >TVA</th>
             <th class="text-center" >Pays</th>
             <th class="text-center" >Type</th>
+            <th class="text-center" ></th>          
         </tr>
     </thead>
     <tbody>
@@ -58,6 +68,8 @@
             <td></td>
             <td></td>
             <td></td>
+            <td></td>
+            <td class="text-center"><button class="btn btn-light"><img src="./assets/img/poubelle.png" alt="poubell" width="20" height="20" ></button></td>
         </tr>
 
     </tbody>
