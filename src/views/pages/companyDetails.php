f320abd1112c3a950351136e390b38e0e95f1aa3
Fidelys <nadisonsolojeanfidelys@gmail.com>
2022-05-20 12:33:59 +0200

    quelque rectification

--- src/views/pages/companyDetails.php
+++ src/views/pages/companyDetails.php
@@ -1,44 +1,46 @@
 <?php ob_start(); ?>
 
-<h1>Société : <span></span> </h1>
-<p>N° TVA : <span></span> </p>
-<p>Type : Client</p>
+<div class="container">
+    <h1>Société : <span></span> </h1>
+    <p>N° TVA : <span></span> </p>
+    <p>Type : Client</p>
 
-<table class= "table table-bordered caption-top">
+    <table class= "table table-bordered caption-top">
 
-    <caption class= "mb-3 fs-2">Personnes de contact</caption>
-    <thead>
-        <tr>
-            <th class="text-center fw-bold" > Nom</th>
-            <th class="text-center fw-bold" >Phone</th>
-            <th class="text-center fw-bold" >Email</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </tbody>
-</table>
+        <caption class= "mb-3 fs-2">Personnes de contact</caption>
+        <thead>
+            <tr>
+                <th class="text-center fw-bold" > Nom</th>
+                <th class="text-center fw-bold" >Phone</th>
+                <th class="text-center fw-bold" >Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
 
-<table class= "table table-bordered caption-top">
-    <caption class= "mb-3 fs-2" >Factures</caption>
-    <thead>
-        <tr>
-            <th class="text-center fw-bold" >N° facture</th>
-            <th class="text-center fw-bold" >Date</th>
-            <th class="text-center fw-bold" >Personne de contact</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td></td>
-        </tr>
-        
-    </tbody>
-</table>
+    <table class= "table table-bordered caption-top">
+        <caption class= "mb-3 fs-2" >Factures</caption>
+        <thead>
+            <tr>
+                <th class="text-center fw-bold" >N° facture</th>
+                <th class="text-center fw-bold" >Date</th>
+                <th class="text-center fw-bold" >Personne de contact</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+            
+        </tbody>
+    </table>
+</div>
 
 <?php
 $content = ob_get_clean();
--- src/views/pages/invoiceDetails.php
+++ src/views/pages/invoiceDetails.php
@@ -1,35 +1,47 @@
 <?php ob_start(); ?>
+    <div class="container">
 
-<h1>Facture : <span></span> </h1>
+    <h1>Facture : <span></span> </h1>
 
-<table>
-    <caption>Société liée à la facture</caption>
-    <thead>
-        <tr>
-            <th>Nom</th>
-            <th>TVA</th>
-            <th>Type société</th>
-        </tr>
-    </thead>
-    <tbody>
+    <table class= "table table-bordered caption-top" >
+        <caption class= "mb-3 fs-2" >Société liée à la facture</caption>
+        <thead>
+            <tr>
+                <th class="text-center fw-bold" >Nom</th>
+                <th class="text-center fw-bold" >TVA</th>
+                <th class="text-center fw-bold" >Type société</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
 
-    </tbody>
-</table>
+        </tbody>
+    </table>
 
 
-<table>
-    <caption>Personne de contact</caption>
-    <thead>
-        <tr>
-            <th>Nom</th>
-            <th>Email</th>
-            <th>Phone</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-    </tbody>
-</table>
+    <table class= "table table-bordered caption-top" >
+        <caption>Personne de contact</caption>
+        <thead>
+            <tr>
+                <th class="text-center fw-bold" >Nom</th>
+                <th class="text-center fw-bold" >Email</th>
+                <th class="text-center fw-bold" >Phone</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            
+        </tbody>
+    </table>
+</div>
 
 <?php 
 $content = ob_get_clean();
