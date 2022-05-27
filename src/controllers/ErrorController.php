e435c9ef415d6bdae9848c768db06b497b08e1e3
Julie <juptsr.becode@gmail.com>
2022-05-27 15:39:14 +0200

    return to login on errorpage if not connected

--- src/controllers/ErrorController.php
+++ src/controllers/ErrorController.php
@@ -6,6 +6,12 @@ class ErrorController extends Controller
 {
     public function displayView()
     {
+        session_start();
+
+        if ( empty($_SESSION['username']) or empty($_SESSION['user_status']) ) {
+            header('location: /logout');
+        }
+        
         if ( preg_match('/\d+/', explode('=', $_SERVER['REQUEST_URI'])[1]) and substr($_SERVER['REQUEST_URI'], 0, 8) == '/details' ) {
             session_start();
             $page = substr($_SERVER['REQUEST_URI'], 9);
